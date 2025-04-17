65e0dcef302c346aae2cbe2e26c1ad456e7f9d73
Ron Moneta <ronmoneta@mail.tau.ac.il>
2025-04-17 17:12:51 +0300

    Seperated Alex original Demo from my version. Added automatic parsing of scan_doc file

--- Rons/inputs.py
+++ Rons/inputs.py
@@ -7,12 +7,13 @@ from pathlib import Path
 class Inputs:
     def __init__(self, subject: Path):
         self.subject = subject
-        self.t1_path = self.subject / ''  # TODO: does it need to be t1w, or qt1?
-        self.t2_path = self.subject / ''  # TODO: does it need to be t2w, or qt2?
-        self.b0_path = self.subject / ''
-        self.b1_path = self.subject / ''  # TODO: folder for b1map exists, but sometimes empty - if empty assume perfect
-        self.mt_path = self.subject / ''  # TODO: make sure its 31 (skip first one?)
-        self.rnoe_path = self.subject / ''  # TODO: make sure its 31 (skip first one?)
+        self.subject_metadata = self.parse_scan_doc()
+        self.t1_path = self.subject / f'{self.subject_metadata["t1map_number"]}/pdata/4/dicom'  # TODO: does it need to be t1w, or qt1?
+        self.t2_path = self.subject / f'{self.subject_metadata["t2map_number"]}/pdata/2/dicom'  # TODO: does it need to be t2w, or qt2?
+        self.b0_path = self.subject / f'{self.subject_metadata["b0map_number"]}/pdata/1/dicom'
+        self.b1_path = self.subject / f'{self.subject_metadata["b1map_number"]}/pdata/1/dicom'  # TODO: folder for b1map exists, but sometimes empty - if empty assume perfect
+        self.mt_path = self.subject / f'{self.subject_metadata["mt_number"]}/pdata/1/dicom'  # TODO: make sure its 31 (skip first one?)
+        self.rnoe_path = self.subject / f'{self.subject_metadata["rNOE_number"]}/pdata/1/dicom'  # TODO: make sure its 31 (skip first one?)
         self.t1_wm_mask_path = self.subject / ''  # ignored for now
         self.t1_gm_mask_path = self.subject / ''  # ignored for now
         self.t1_map = self.load_auxiliary_data(self.t1_path, '3')  # TODO: Change to generic
@@ -21,9 +22,9 @@ class Inputs:
         self.b1_map = self.load_auxiliary_data(self.b1_path, '2')  # TODO: Change to generic
         self.mt_map = self.load_mrf_data(self.mt_path)
         self.rnoe_map = self.load_mrf_data(self.rnoe_path)
-        self.dataset = xr.Dataset(
+        self.dataset = xr.Dataset(  # TODO: Alex's code has AMIDE_data hardcoded, change when possible
             {'roi_mask_nans': self.t1_map, 'B1_fix_factor_map': self.b1_map, 'B0_shift_ppm_map': self.b0_map,
-             'T2ms': self.t2_map, 'T1ms': self.t1_map, 'MT_data': self.mt_map, 'rNOE_data': self.rnoe_map})
+             'T2ms': self.t2_map, 'T1ms': self.t1_map, 'MT_data': self.mt_map, 'AMIDE_data': self.rnoe_map})
 
     def load_auxiliary_data(self, map_path: Path, echo: str) -> xr.DataArray:
         """
@@ -48,3 +49,39 @@ class Inputs:
         mrf_data = np.expand_dims(mrf_data, axis=2)  # Add a new axis to the array
         mrf_data = np.repeat(mrf_data, 4, axis=2)  # Repeat the array along the new axis
         return xr.DataArray(mrf_data, dims=("MRF_cycles", "height", "slice", "width"))
+
+    def parse_scan_doc(self) -> {str: {str: str}}:
+        """
+        Given a subject path, find the scan_doc.csv file and parse it into a dictionary.
+        specifically, create a dictionary with the following keys:
+        'subject_path': {
+            't1map_number': str,
+            't2map_number': str,
+            'b0map_number': str,
+            'b1map_number': str,
+            'mt_number': str,
+            'rNOE_number': str
+            }
+        The scan_doc.csv file should have the following format:
+        'export_idx','scan category','scan name','scan duration','scan dim'
+        and the inner dictionary should be created by parsing the 'scan name' column in this manner:
+        'scan name' == 't1_map' -> 'export_idx' == 't1map_number'
+        'scan name' == 't2_map' -> 'export_idx' == 't2map_number'
+        'scan name' == 'wasser' -> 'export_idx' == 'b0map_number'
+        'scan name' == 'B1map' -> 'export_idx' == 'b1map_number'
+        'scan name' == '52_MT' -> 'export_idx' == 'mt_number'
+        'scan name' == '51_rnoe' -> 'export_idx' == 'rNOE_number'
+        :return: dictionary of subject scan metadata
+        """
+        scan_doc_path = self.subject / 'scan_doc.csv'
+        with open(scan_doc_path, 'r') as file:
+            scan_doc = {line.split(',')[2]: line.split(',')[0] for line in file}
+            subject_metadata = {
+                't1map_number': scan_doc.get('t1_map', None),
+                't2map_number': scan_doc.get('t2_map', None),
+                'b0map_number': scan_doc.get('wasser', None),
+                'b1map_number': scan_doc.get('B1map', None),
+                'mt_number': scan_doc.get('52_MT', None),
+                'rNOE_number': scan_doc.get('51_rnoe', None)
+            }
+        return subject_metadata
