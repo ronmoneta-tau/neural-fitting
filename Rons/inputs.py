74645413a1439b9a09c7c9eb312cf022c1993275
Ron Moneta <ronmoneta@mail.tau.ac.il>
2025-04-17 16:33:08 +0300

    Added creation of DataArrays and DataSet

--- Rons/inputs.py
+++ Rons/inputs.py
@@ -1,6 +1,8 @@
 import numpy as np
+import xarray as xr
 import pydicom
 from pathlib import Path
+
 
 class Inputs:
     def __init__(self, subject: Path):
@@ -13,33 +15,36 @@ class Inputs:
         self.rnoe_path = self.subject / ''  # TODO: make sure its 31 (skip first one?)
         self.t1_wm_mask_path = self.subject / ''  # ignored for now
         self.t1_gm_mask_path = self.subject / ''  # ignored for now
-        self.t1_map = self.load_auxiliary_data(self.t1_path, '3') # TODO: Change to generic
-        self.t2_map = self.load_auxiliary_data(self.t2_path, '3') # TODO: Change to generic
-        self.b0_map = self.load_auxiliary_data(self.b0_path, '02') # TODO: Change to generic
-        self.b1_map = self.load_auxiliary_data(self.b1_path, '2') # TODO: Change to generic
+        self.t1_map = self.load_auxiliary_data(self.t1_path, '3')  # TODO: Change to generic
+        self.t2_map = self.load_auxiliary_data(self.t2_path, '3')  # TODO: Change to generic
+        self.b0_map = self.load_auxiliary_data(self.b0_path, '02')  # TODO: Change to generic
+        self.b1_map = self.load_auxiliary_data(self.b1_path, '2')  # TODO: Change to generic
         self.mt_map = self.load_mrf_data(self.mt_path)
         self.rnoe_map = self.load_mrf_data(self.rnoe_path)
+        self.dataset = xr.Dataset(
+            {'roi_mask_nans': self.t1_map, 'B1_fix_factor_map': self.b1_map, 'B0_shift_ppm_map': self.b0_map,
+             'T2ms': self.t2_map, 'T1ms': self.t1_map, 'MT_data': self.mt_map, 'rNOE_data': self.rnoe_map})
 
-    def load_auxiliary_data(self, map_path: Path, echo: str) -> np.ndarray:
+    def load_auxiliary_data(self, map_path: Path, echo: str) -> xr.DataArray:
         """
         Loads an echo and inflates it to be of shape (height, 4, width)
         :param map_path: Path to the auxiliary maps
         :param echo: Index of the echo to load
-        :return: Array of inflated echo of the auxiliary map
+        :return: DataArray of inflated echo of the auxiliary map
         """
         echo = pydicom.dcmread(map_path / f'MRIm{echo}.dcm').pixel_array
         echo = np.expand_dims(echo, axis=1)  # Add a new axis to the array
         echo = np.repeat(echo, 4, axis=1)  # Repeat the array along the new axis
-        return echo
+        return xr.DataArray(echo, dims=("height", "slice", "width"))
 
-    def load_mrf_data(self, mrf_path: Path) -> np.ndarray:
+    def load_mrf_data(self, mrf_path: Path) -> xr.DataArray:
         """
         Load the MRF data (31 images) and inflates it to be of shape (31, height, 4, width)
         :param mrf_path: Path to the MRF maps
-        :return: Array of inflated MRF images
+        :return: DataArray of inflated MRF images
         """
         images = [pydicom.dcmread(im).pixel_array for im in mrf_path.glob('MRIm*.dcm') if im.name != 'MRIm01.dcm']
         mrf_data = np.stack(images, axis=0)
         mrf_data = np.expand_dims(mrf_data, axis=2)  # Add a new axis to the array
         mrf_data = np.repeat(mrf_data, 4, axis=2)  # Repeat the array along the new axis
-        return mrf_data
+        return xr.DataArray(mrf_data, dims=("MRF_cycles", "height", "slice", "width"))
